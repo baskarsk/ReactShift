317f22e2f097f35afe7e2fdd3467d650dab021d9
baskarsk <baskar.saravanan@cognizant.com>
2018-10-16 15:53:39 +0530

    modified port to 8080 from 3005

--- tools/server.js
+++ tools/server.js
@@ -6,7 +6,7 @@ import open from 'open';
 
 /* eslint-disable no-console */
 
-const port = process.env.PORT || 3005;
+const port = process.env.PORT || 8080;
 const app = express();
 const compiler = webpack(config);
 
